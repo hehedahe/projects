90270512f92b69b924c5464b7a8d61f6fbc54cc1
hehedahe <smyekgo@gmail.com>
2021-12-30 21:01:18 +0900

    211230

---
+++ project-boot/app/src/main/java/com/mylist/method/Rectangle.java
@@ -0,0 +1,42 @@
+package com.mylist.method;
+
+public class Rectangle {
+
+  int width;
+  int length;
+  String color;
+  int area;
+  int perimeter;
+  public int getWidth() {
+    return width;
+  }
+  public void setWidth(int width) {
+    this.width = width;
+  }
+  public int getLength() {
+    return length;
+  }
+  public void setLength(int length) {
+    this.length = length;
+  }
+  public String getColor() {
+    return color;
+  }
+  public void setColor(String color) {
+    this.color = color;
+  }
+  public int getArea() {
+    return area;
+  }
+  public void setArea(int area) {
+    this.area = area;
+  }
+  public int getPerimiter() {
+    return perimeter;
+  }
+  public void setPerimiter(int perimiter) {
+    this.perimeter = perimiter;
+  }
+
+
+}
--- project-boot/app/src/main/java/com/mylist/method/TestRectangle.java
+++ project-boot/app/src/main/java/com/mylist/method/TestRectangle.java
@@ -1,10 +1,43 @@
 package com.mylist.method;
 
+import java.util.Scanner;
+
 public class TestRectangle {
 
   public static void main(String[] args) {
 
     Scanner keyboard = new Scanner(System.in);
+
+    Rectangle rec1 = new Rectangle();
+    System.out.println("첫 번째 직사각형의 샐깍: ");
+    rec1.setColor(keyboard.next());
+
+    System.out.println("첫 번째 직사각형의 가로: ");
+    rec1.setWidth(keyboard.nextInt());
+
+    System.out.println("첫 번째 직사각형의 세로: ");
+    rec1.setLength(keyboard.nextInt());
+
+
+    Rectangle rec2 = new Rectangle();
+    System.out.println("두 번째 직사각형의 샐깍: ");
+    rec1.setColor(keyboard.next());
+
+    System.out.println("두 번째 직사각형의 가로: ");
+    rec1.setWidth(keyboard.nextInt());
+
+    System.out.println("첫 번째 직사각형의 세로: ");
+    rec1.setLength(keyboard.nextInt());
+
+    System.out.println(rec1.getColor() + "직사각형의 넓이는 " + rec1.area() 
+    + "이고, 둘레는 " + rec1.perimeter() + "입니다.");
+
+    System.out.println(rec2.getColor() + "직사각형의 넓이는 " + rec2.area() 
+    + "이고, 둘레는 " + rec2.perimeter() + "입니다.");
+
+    // 두 직사각형의 넓이 비교하기
+
+    // 두 직사각형의 둘레 비교하기
 
   }
 
